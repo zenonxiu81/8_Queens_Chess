f62768935dab51a399a486eb327ab648f7bc0f44
zenonxiu81 <69152239+zenonxiu81@users.noreply.github.com>
2021-12-11 17:33:45 +0800

    C code for solving 8 queens on Chess board issue

---
+++ 8_queens.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define NUM (8)  //number of queens, can be changed to other numbers
+
+
+void print_martix(int M[NUM][NUM])
+{
+ int i, j;
+ for(i=0;i<NUM;i++)
+   {
+		for(j=0;j<NUM;j++)
+		  printf("%2d ", M[i][j]);
+
+		printf("\n");	 
+	 }	
+
+	 	printf("*********************\n");	 
+}
+
+
+void copy_martix(int * p_src, int * p_dst)
+{
+ int i, j;
+ for(i=0;i<NUM;i++)
+   {
+		for(j=0;j<NUM;j++)
+		  *(p_dst+NUM*i+j)=*(p_src+NUM*i+j);
+	 }	
+}
+
+
+void go_through(int M[NUM][NUM], int line)
+{
+  int M1[NUM][NUM];
+  int i,j,k, m,n;
+
+  if(line>=NUM)   //run out of lines?
+	 return ;
+
+  for(i=0;i<NUM;i++)
+  {	
+   if(M[line][i]==TRUE)
+   {
+	  if(line==NUM-1)   //if reachs to the last line, and there is a TRUE 
+	   {
+	    //printf("found one match\n");
+		 print_martix(M);
+		 continue;
+	 	}
+	 else
+	 {
+	 copy_martix(&M[0][0],&M1[0][0]);  //copy M to M1
+
+	 /*begining of setting other elemetns of cross lines to false */
+
+	 for(j=0;j<NUM;j++)    //set the row false
+	   {
+		 if(j!=i)
+		   M1[line][j]=FALSE;
+	  }
+	 for(j=0;j<NUM;j++)    //set the column to false
+	  {
+		 if(j!=line)
+		   M1[j][i]=FALSE;
+	  }
+
+     m=i+1;  //set top_right elements to false
+	 n=line-1;
+	 while(n>=0&&m<NUM) 
+	   {
+			M1[n][m]=FALSE;
+			n=n-1;
+			m=m+1;
+		 }
+
+     m=i-1;   //set top_left elements to false
+	 n=line-1;
+	 while(n>=0&&m>=0) //set top_right elements to false
+	   {
+			M1[n][m]=FALSE;
+			n=n-1;
+			m=m-1;
+		 }
+
+	 m=i-1;   //set bottom_left false
+	 n=line+1;
+	 while(n<NUM&&m>=0) //set top_right false
+	   {
+			M1[n][m]=FALSE;
+			n=n+1;
+			m=m-1;
+		 }	 
+
+	 m=i+1;   //set bottom_right false
+	 n=line+1;
+	 while(n<NUM&&m<NUM) //set top_right false
+	   {
+			M1[n][m]=FALSE;
+			n=n+1;
+			m=m+1;
+		 }	 	 
+	
+	/* end of setting other elemetns of cross lines to false */	 
+	
+	line++;	 //move to next line
+
+	go_through(M1,line); //run throgh possibility of next line
+
+	line--; //move back to current line
+
+	 }
+		
+   }	   
+  }
+}
+
+int main()
+{
+ int Matrix[NUM][NUM];	
+ int i, j;
+ for(i=0;i<NUM;i++)
+   {
+		for(j=0;j<NUM;j++)
+		  Matrix[i][j]=TRUE; 
+	 }	
+ go_through(Matrix,0);
+ return 0;
+}
